c1bfc8df80012d2c82d3702d79ba6e2f4e096c7e
raghav_chalapathy <ragha.mane@gmail.com>
2018-06-10 15:15:41 +1000

    Update lottery.sol

--- lottery.sol
+++ lottery.sol
@@ -3,11 +3,29 @@ pragma solidity ^0.4.17;
 contract Lottery{
 
     address public manager;
+    address[] public players;
     
     function Lottery() public { 
         manager = msg.sender;
-        
     }
+    
+    function enter() public payable {
+      require(msg.value > 0.01 ether);
+      players.push(msg.sender);  
+    }
+    
+    function random() private view returns (uint256) {
+      return uint256(sha3(block.difficulty,now,players));
+    }
+    
+    function pickWinner() public {
+        require(msg.sender == manager);
+        uint256 index = random() % players.length;
+        //this refers to the current contract
+        players[index].transfer(this.balance); // 0x1083289473920437289438473473984 send money from contract
+        players = new address[](0); // reset the lottery we want to create an dynamic array of initial size of 0 length of array is 0        
+    }
+    
+    
 
 }
-
