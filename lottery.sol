d0fff27812afda306df5f06c263d1d23943c7ae3
raghav_chalapathy <ragha.mane@gmail.com>
2018-06-13 07:37:36 +1000

    Update lottery.sol

--- lottery.sol
+++ lottery.sol
@@ -4,36 +4,37 @@ contract Lottery{
 
     address public manager;
     address[] public players;
-    
-    function Lottery() public { 
+
+    function Lottery() public {
         manager = msg.sender;
     }
-    
+
     function enter() public payable {
-      require(msg.value > 0.01 ether);
-      players.push(msg.sender);  
+        require(msg.value > 0.01 ether);
+        players.push(msg.sender);
     }
-    
+
     function random() private view  returns (uint256) {
-      return uint256(sha3(block.difficulty,now,players));
+        return uint256(sha3(block.difficulty,now,players));
     }
-    
+
     function pickWinner() restricted public {
         require(msg.sender == manager);
         uint256 index = random() % players.length;
         //this refers to the current contract
         players[index].transfer(this.balance); // 0x1083289473920437289438473473984 send money from contract
-        players = new address[](0); // reset the lottery we want to create an dynamic array of initial size of 0 length of array is 0        
+        players = new address[](0); // reset the lottery we want to create an dynamic array of initial size of 0 length of array is 0
     }
-    
+
     function getPlayers() public view returns (address[]){
         return players;
     }
-    
+
     modifier restricted() { // only manager can call
         require(msg.sender==manager);
         _; // early validation logic
     }
-    
+
 
 }
+
